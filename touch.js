bf6981e90e993c189c6104a28960c1d96ca3ae71
Geordie Kaytes <geordie.kaytes@freshtilledsoil.com>
2013-11-11 14:23:26 -0500

    works

--- touch.js
+++ touch.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
 	var indicator = $("<div class='touch-indicator'></div>");
-	var touch;
+	var indicatorInstance;
 
-	$(".touch-area").on("touchstart ", function(){
+	$(".touch-area").on("touchstart", function(){
 		$(this).addClass("touching");
 	}).on("touchend", function(){
 		$(this).removeClass("touching");
 	});
 
 	$("body").on("touchstart", function(e){
-		touch = e.touches[0];
-		indicator.appendTo(document.body).css({
+		e.preventDefault();
+		var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+		indicatorInstance = indicator.appendTo(document.body).css({
 			"top": touch.pageY-8+"px",
 			"left": touch.pageX-8+"px"
 		});
-	}).on('touchend', function(){
-		indicator.remove();
+
+	}).on('touchmove', function(e){
+		e.preventDefault();
+		var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+		indicatorInstance.css({
+			"top": touch.pageY-8+"px",
+			"left": touch.pageX-8+"px"
+		});
+	}).on('touchend', function(e){
+		e.preventDefault();
+		indicatorInstance.remove();
 	});
 });
